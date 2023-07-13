bc058804f68b5ed0f2315c9757ab25897498adae
HUANG SIZHE <hsz1273327@gmail.com>
2023-07-13 14:40:55 +0800

    update

--- gpubase-notebook/docker-stacks-foundation/build.sh
+++ gpubase-notebook/docker-stacks-foundation/build.sh
@@ -1,1 +1,1 @@
-docker buildx build --push --platform=linux/amd64,linux/arm64 -t hsz1273327/docker-stacks-foundation-cuda:11.8.0-cudnn8-ubuntu22.04 -t hsz1273327/docker-stacks-foundation-cuda:latest .
+docker buildx build --push --platform=linux/amd64,linux/arm64 -t hsz1273327/gpu-docker-stacks-foundation:cuda11.8.0-cudnn8-ubuntu22.04 -t hsz1273327/gpu-docker-stacks-foundation:latest .
