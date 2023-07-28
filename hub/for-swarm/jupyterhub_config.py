56d2f804e99408c05fa238c2643bc09158defddd
HUANG SIZHE <hsz1273327@gmail.com>
2023-07-28 18:39:34 +0800

    Update jupyterhub_config.py

--- hub/for-swarm/jupyterhub_config.py
+++ hub/for-swarm/jupyterhub_config.py
@@ -432,13 +432,13 @@ if constraint_gpus:
                 if use_gpus_count != 0:
                     extra_resources_spec = {
                         "generic_resources": {
-                            'gpu': use_gpus_count
+                            'NVIDIA-GPU': use_gpus_count
                         }
                     }
                 else:
                     extra_resources_spec = {
                         "generic_resources": {
-                            'gpu': use_gpus_device_id
+                            'NVIDIA-GPU': use_gpus_device_id
                         }
                     }
                 constraint_gpu_map[suffix] = extra_resources_spec
